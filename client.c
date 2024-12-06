0c5e35a82fd4f41c38f3565da0f0a5d311324883
Tomas Bruha <tomasbruha1@gmail.com>
2024-12-07 00:14:55 +0100

    start of char_to_binary

--- client.c
+++ client.c
@@ -6,19 +6,60 @@
 /*   By: tbruha <tbruha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 17:57:40 by tbruha            #+#    #+#             */
-/*   Updated: 2024/12/06 12:02:26 by tbruha           ###   ########.fr       */
+/*   Updated: 2024/12/07 00:13:46 by tbruha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	send_end()
+{
+	
+}
+
+void	char_to_binary(pid_t pid, char *str)
+{
+	int	bites;
+	int	i;
+	
+	i = 0;
+	ft_printf("%s\n", str);
+	ft_printf("pid: %d\n", pid);
+	while (str[i] != '\0')
+	{
+		bites = 7;
+		while (bites > 0)
+		{
+			if (((str[i] >> bites) & 1) == 0)
+			{
+				ft_printf("0");
+				kill(pid, SIGUSR1);
+			}
+			else
+			{
+				ft_printf("1");
+				kill(pid, SIGUSR2);
+			}
+			bites--;
+		}
+		i++;
+		write(1, "\n", 1);
+	}
+	send_end();
+}	
+
 int	main(int argc, char **argv)
 {
-	if (argc != 2)
-		return (1);
-	kill(atoi(argv[1]), SIGUSR1);
+	char *str;
+
+	if (argc != 3)
+		return (1);	
 	ft_printf("Running....\n");
-	while (1)
-		pause();
+	str = argv[2];
+	char_to_binary(ft_atoi(argv[1]), str);
+
+	// while (1)
+	// 	pause();
+	// // wait for a 'OK' response after sending the terminating char.
 	return (0);
 }
